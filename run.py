d635f28aab79932637de567fe2e2b754bba09b75
Mehrdad Lashgari <mehr.lashgari@gmail.com>
2022-02-09 22:29:30 +0000

    very first structure based on what i saw from the guidance

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+class Board:
+    
+
+
+    def __init__(seld,):
+
+    def print():
+
+    def guess():
+
+    def add_ship():
+
+    
+def random_print(size):
+
+
+def valid_coordination():
+
+def populate_board():
+
+def make_guess():
+
+def play_game():
+
+def new_game():
+
